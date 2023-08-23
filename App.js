d192f8b0088ebf999b09c5e1b057f907d0a0d462
Hamza Kamelen <78796144+hamzakamelen@users.noreply.github.com>
2023-08-24 01:09:35 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import { Progress} from "@chakra-ui/react";
+<Box>
+        <Progress
+          colorScheme="blue"
+          hasStripe
+          height="42px"
+          value={100}
+          isAnimated
+        />
+        <Text
+          color={"white"}
+          fontSize={{ base: "80%", sm: "100%", lg: "100%" }}
+          position="absolute"
+          top={{ base: "117px", sm: "115px", md: "142px", lg: "85px" }}
+          left={{ base: "5%", sm: "27%", md: "30%", lg: "40%" }}
+        >
+          New arrivals in watches upto 30% off ❤️
+        </Text>
+      </Box>
